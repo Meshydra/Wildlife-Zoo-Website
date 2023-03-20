01416f8002fe29719d240f8788c9cfeaa1621eb2
Meshith Ariyawansa <71543277+Meshydra@users.noreply.github.com>
2023-03-20 15:26:51 +0530

    Functioning page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './components/pages/Home'
 
 function App() {
   return (
     <>
-    <Router>      
-      <Navbar />
-      <Routes>
-        <Route path='/' exact />
-      </Routes>
-        
-    </Router>
+      <Router>      
+        <Navbar />
+        <Routes>
+          <Route path='/' exact Component={Home} />
+        </Routes>        
+      </Router>
     </>
   );
 }
