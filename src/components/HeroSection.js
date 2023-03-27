81e2ffbe78b3b72bb1c362751aa7fe1d65848ff2
Meshith Ariyawansa <71543277+Meshydra@users.noreply.github.com>
2023-03-27 18:59:36 +0530

    Finished the Home page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,15 +6,15 @@ import '../App.css'
 export default function HeroSection() {
     return (
     <div className='hero-container'>
-        <video src="/videos/video-2.mp4" autoPlay loop muted />
+        <video src="/videos/hero.mp4" autoPlay loop muted />
         <h1>Welcome to the Zoo</h1>
-        <p>More information can be found below. </p>
+        <p>A Zoo where we obeserve the wildlife not trap it </p>
         <div className='hero-btns'>
             <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
                 Animals
             </Button>
             <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
-                Contact Us <i className='fab '></i>
+                Contact<i className='fab '></i>
             </Button>
         </div>
     </div>
