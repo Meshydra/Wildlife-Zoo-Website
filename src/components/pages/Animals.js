17ffe7a978a3559606332af83d36f91be5321b54
Meshith Ariyawansa <71543277+Meshydra@users.noreply.github.com>
2023-04-14 14:26:35 +0530

    Css edits

--- src/components/pages/Animals.js
+++ src/components/pages/Animals.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function Animals() {
-    return <h1 className='products'>Animals</h1>;
+    return <h1 className='animals'>Animals</h1>;
 }
