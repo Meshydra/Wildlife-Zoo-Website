97b711602851f61ec681e95eb46288ab226b4c6c
Meshith Ariyawansa <71543277+Meshydra@users.noreply.github.com>
2023-04-15 14:27:31 +0530

    Css editing

--- src/components/pages/Merch.js
+++ src/components/pages/Merch.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function Merch() {
-    return <h1 className='services'>Merch</h1>;
+    return <h1 className='merch'>Merch</h1>;
 }
